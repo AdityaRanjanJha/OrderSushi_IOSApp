ca530bdbbe5229d353b7e8d3740485fe4248a6aa
Aditya Ranjan Jha <adityaranjan2014@gmail.com>
2023-03-19 23:48:04 +0530

    Initial Commit

---
+++ OrderSushi_IOSApp/OrderSushi_IOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderSushi_IOSAppApp.swift
+//  OrderSushi_IOSApp
+//
+//  Created by Aditya Ranjan Jha on 19/03/23.
+//
+
+import SwiftUI
+
+@main
+struct OrderSushi_IOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
